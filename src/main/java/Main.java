aca1d786825bbc11f99e84f7a1579b2774e13cf1
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-08-21 19:02:08 -0500

    added readme.md and screenshot

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import ui.UiMenu;
-
-public class Main {
-    public static void main(String[] args) {
-        ui.UiMenu.showMainMenu();
-    }
-}
--- src/users/Customer.java
+++
@@ -1,7 +0,0 @@
-package users;
-
-public class Customer extends User {
-    public Customer(String firstName, String lastName, Long idNumber) {
-        super(firstName, lastName, idNumber);
-    }
-}
